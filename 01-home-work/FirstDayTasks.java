a0af8b993a0ccd97545fa70413e3111ec70c54fa
Tilegen <tilegenjuban@gmail.com>
2025-04-09 16:59:33 +0500

    1 task

--- 01-home-work/FirstDayTasks.java
+++ 01-home-work/FirstDayTasks.java
@@ -4,6 +4,19 @@ import java.util.stream.Collectors;
 
 public class FirstDayTasks {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        List<String> words = Arrays.stream(sentence
+                        .replaceAll("[^\\p{L}\\p{N}]+", " ")
+                        .split("\\s+"))
+                .map(String::toLowerCase)
+                .toList();
+        Map<String, Long> wordCount = words.stream()
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+
+        wordCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .forEach(e -> System.out.println(e.getKey()));
 
     }
 }
